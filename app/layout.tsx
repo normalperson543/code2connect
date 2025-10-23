2d2cc147ca8ea201344bf64570a90fbd8257cf1b
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-10-23 16:07:56 -0700

    linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Manrope, Public_Sans } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
 import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
